0542b042fd6e139d813bdd46af38f27f5a8cd742
Danita <159792265+iamatinad@users.noreply.github.com>
2025-05-02 22:28:37 +0000

    Update index.js

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -19,7 +19,7 @@ import ati from "./images/projects/ati.png";
 import bd from "./images/projects/bd.jpg";
 import edc from "./images/projects/edc.jpeg";
 import fit from "./images/projects/fitness.jpg";
-import home from "./images/projects/Home.jpg";
+import home from "./images/projects/home.png";
 
 export {
   edc,
