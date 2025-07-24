fcb8b71d3b7af365e67518dadf6e076284342f8e
Danita <159792265+iamatinad@users.noreply.github.com>
2025-07-24 13:21:22 +0000

    Update leftbanner.js

--- frontend/src/components/banner/leftbanner.js
+++ frontend/src/components/banner/leftbanner.js
@@ -16,7 +16,7 @@ const LeftBanner = () => {
         <h4 className=" text-lg font-normal">WELCOME TO MY DYNAMIC WORLD</h4>
         <h1 className="text-6xl font-bold text-white">
           Hi, I'm{" "}
-          <span className="text-designColor capitalize">Danita Lamptey</span>
+          <span className="text-designColor capitalize">Danita </span>
         </h1>
         <h2 className="text-4xl font-bold text-white">
           a <span>{text}</span>
